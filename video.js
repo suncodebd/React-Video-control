ab85c1dd607ead202b3805db3019aa96cba59743
Md Minarul islam <101469612+suncodebd@users.noreply.github.com>
2022-06-15 08:47:31 +0600

    this is custom video  control in react code d
    Awesome code for video control in react and custom csss

---
+++ video.js
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
+
+function Intro() {
+    const [playVideo, setPlayVideo] = useState(false);
+    const vidRef = useRef();
+
+    const handleVideo = () => {
+        setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+        if (playVideo) {
+            vidRef.current.pause();
+        } else {
+            vidRef.current.play();
+        }
+    };
+
+    return (
+        <div className="app__video">
+            <video ref={vidRef} src={meal} type="video/mp4" loop controls={false} muted />
+            <div className="app__video-overlay flex__center">
+                <div
+                    onClick={handleVideo}
+                    className="app__video-circle flex__center"
+                    tabIndex="0"
+                    role="button"
+                >
+                    {playVideo ? (
+                        <BsPauseFill color="#fff" fontSize={30} />
+                    ) : (
+                        <BsFillPlayFill color="#fff" fontSize={30} />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Intro;
